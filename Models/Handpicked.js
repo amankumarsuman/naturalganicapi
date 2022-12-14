606ef2b97da435ba8b951f2d0143cca5d34423f8
akumar <akumar@comtronusa.com>
2022-12-14 00:40:56 -0500

    added upload part

--- Models/Handpicked.js
+++ Models/Handpicked.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const handpickedSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: { type: String },
-  desc: { type: Number },
-  date: { type: Number },
+  desc: { type: String },
+  date: { type: String },
 
   image: { type: String },
 });
